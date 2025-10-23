bb23d62e298ee204a1d83de039b40b8dea5b1606
nauraistizahra <nauraistizahra@gmail.com>
2025-10-23 15:39:42 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,7 @@
+kendaraan = ["namakendaraan", "jenis kendaraan", "cckendaraan", "warna kendaraan", "rodakendaraan"]
+
+kendaraan.extend(["hargakendaraa", "tipekendaraan"])
+
+kendaraan.insert(2, "merkkendaraan")
+
+print(kendaraan)
